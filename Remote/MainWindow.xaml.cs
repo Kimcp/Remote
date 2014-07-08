8dc5d6d9b1d98d99c1c4fa52ddfd81537faac0e3
Kimcp <sasinvs@naver.com>
2014-07-09 02:45:19 +0900

    add data class - second

--- Remote/MainWindow.xaml.cs
+++ Remote/MainWindow.xaml.cs
@@ -20,9 +20,62 @@ namespace Remote
     /// </summary>
     public partial class MainWindow : Window
     {
+        //environment variable
+        public double Length { get; set; }
+        public double Velocity { get; set; }
+        public double StartScope { get; set; }
+        public double FinishScope { get; set; }
+        public double Delay { get; set; }
+        public FilterType Filter { get; set; }
+
+        //simulation variable
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        private void InitializingParameter()
+        {
+            try
+            {
+                this.Length = double.Parse(length.Text);
+                this.Velocity = double.Parse(velocity.Text);
+                this.StartScope = double.Parse(startscope.Text);
+                this.FinishScope = double.Parse(finishscope.Text);
+                this.Delay = double.Parse(delay.Text);
+                this.Filter = (FilterType)filter.SelectedIndex;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("잘못된 입력!\r\n"+e.ToString());
+                InitializingInput();
+            }
+        }
+
+        private void InitializingInput()
+        {
+            length.Text = "0";
+            velocity.Text = "0";
+            startscope.Text = "0";
+            finishscope.Text = "0";
+            delay.Text = "0";
+            filter.SelectedIndex = 0;
+        }
+
+        //test click event
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            InitializingParameter();
+        }
+    }
+
+    public enum FilterType
+    {
+        Kalman,        
+        LinearRegression,
+        LowPass
     }
 }
--- Remote/data.cs
+++ Remote/data.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Remote
 {
-    class data
+    public enum PointType
     {
+        Transmission,
+        reception
+    }
+
+    public class data
+    {
+        public PointType TransmissionPoint { get; set; }
+        public Point EstimatedPoint { get; set; }
+        public Point PredictedPoint { get; set; }
     }
 }
