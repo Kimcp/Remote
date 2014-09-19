9715b3357f2eb6f9d461b75caa6e0a710d8e7cd3
Kimcp <sasinvs@naver.com>
2014-09-19 10:25:36 +0900

    add additional filter code

--- Remote/FilterClass.cs
+++ Remote/FilterClass.cs
@@ -10,17 +10,38 @@ namespace Remote
     public class FilterClass
     {
         public int WindowSize { get; set; }
+        public double tick { get; set; }
         private Queue<Point> DataQueue;
+
+        Point prv;
 
         public FilterClass()
         {
             WindowSize = 0;
+            tick = 0;
             DataQueue = new Queue<Point>();
         }
 
-        public virtual Point Estimate() 
+        public virtual Point Estimate()
         {
-            return new Point(this.DataQueue.Last().X+5, this.DataQueue.Last().Y+5);
+            Point result;
+
+            if (DataQueue.Count > 1)
+            {
+                Point dff = diff();
+                result = new Point(this.DataQueue.Last().X + 5, this.DataQueue.Last().Y + dff.Y * 0.2);
+            }
+            else
+            {
+                result = new Point(this.DataQueue.Last().X + 5, this.DataQueue.Last().Y + 5);
+            }
+            prv = this.DataQueue.Last();
+            return result;
+        }
+
+        private Point diff()
+        {
+            return new Point(this.DataQueue.Last().X - prv.X, this.DataQueue.Last().Y - prv.Y);
         }
 
         public void Add(Point p)
--- Remote/MainWindow.xaml.cs
+++ Remote/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Remote
 {
@@ -26,16 +27,31 @@ namespace Remote
         public double StartScope { get; set; }
         public double FinishScope { get; set; }
         public double Delay { get; set; }
+        public double Direction { get; set; }
         public FilterType Filter { get; set; }
-        public FilterClass SelectedFilter;
+        public FilterClass SelectedFilter1;
+        public FilterClass SelectedFilter2;
+        public long ScanFreq { get; set; }
+        public long FreqTick { get; set; }
+
+        public Point Target { get; set; }
 
         //simulation variable
+        DispatcherTimer MainTimer = new DispatcherTimer();
+        Queue<data> Queue1 = new Queue<data>();
+        Queue<data> Queue2 = new Queue<data>();
 
 
         public MainWindow()
         {
             InitializeComponent();
-            SelectedFilter = new FilterClass();
+            SelectedFilter1 = new FilterClass();
+            SelectedFilter2 = new FilterClass();
+            MainTimer.Tick += new EventHandler(TimerEvent);
+            ScanFreq = 10000000;
+            FreqTick = 1000000;
+            MainTimer.Interval = new TimeSpan(ScanFreq);
+            InitializingInput();
         }
 
         private void InitializingParameter()
@@ -47,11 +63,12 @@ namespace Remote
                 this.StartScope = double.Parse(startscope.Text);
                 this.FinishScope = double.Parse(finishscope.Text);
                 this.Delay = double.Parse(delay.Text);
+                this.Direction = double.Parse(direction.Text);
                 this.Filter = (FilterType)filter.SelectedIndex;
             }
             catch (Exception e)
             {
-                MessageBox.Show("잘못된 입력!\r\n"+e.ToString());
+                MessageBox.Show("잘못된 입력!\r\n" + e.ToString());
                 InitializingInput();
             }
         }
@@ -59,17 +76,21 @@ namespace Remote
         private void InitializingInput()
         {
             length.Text = "0";
-            velocity.Text = "0";
-            startscope.Text = "0";
-            finishscope.Text = "0";
+            velocity.Text = "50";
+            startscope.Text = "10000";
+            finishscope.Text = "10000";
+            direction.Text = "30";
             delay.Text = "0";
             filter.SelectedIndex = 0;
         }
+
+
 
         //test click event
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             InitializingParameter();
+            MainTimer.Start();
         }
 
         private void filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -79,19 +100,100 @@ namespace Remote
 
             }
 
-            else if((FilterType)filter.SelectedIndex == FilterType.LinearRegression)
+            else if ((FilterType)filter.SelectedIndex == FilterType.LinearRegression)
             {
 
             }
-            else if((FilterType)filter.SelectedIndex == FilterType.LinearRegression)
+            else if ((FilterType)filter.SelectedIndex == FilterType.LinearRegression)
             {
 
             }
             else
             {
-                SelectedFilter = new FilterClass();
+                SelectedFilter1 = new FilterClass();
+                SelectedFilter2 = new FilterClass();
             }
         }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            EstimateTarget();
+            MoveTarget();
+        }
+
+        private void EstimateTarget()
+        {
+            SelectedFilter1.Add(new Point(Target.X, Target.Y));
+            SelectedFilter2.Add(new Point(Target.X, Target.Y));
+            if (Math.Sqrt(Target.X * Target.X + Target.Y * Target.Y) <= StartScope)
+            {
+                Point tmp = SelectedFilter1.Estimate();
+                data remotedata = new data();
+                remotedata.EstimatedPoint = new Point(Target.X, Target.Y);
+                remotedata.PredictedPoint = tmp;
+                Queue1.Enqueue(remotedata);
+                start.Children.Add(MakeEllipse(Target.X, Target.Y, start.Height));
+                start.Children.Add(MakeRectangle(remotedata.PredictedPoint.X, remotedata.PredictedPoint.Y, start.Height));
+            }
+            if (Math.Sqrt((Target.X - Length) * (Target.X - Length) + Target.Y * Target.Y) <= FinishScope)
+            {
+                Point tmp = SelectedFilter2.Estimate();
+                data remotedata = new data();
+                remotedata.EstimatedPoint = new Point(Target.X, Target.Y);
+                remotedata.PredictedPoint = tmp;
+                Queue2.Enqueue(remotedata);
+                finish.Children.Add(MakeEllipse(Target.X, Target.Y, finish.Height));
+                finish.Children.Add(MakeRectangle(remotedata.PredictedPoint.X, remotedata.PredictedPoint.Y, finish.Height));
+            }
+        }
+
+        private void PredictTarget()
+        {
+
+        }
+
+        private void DisplayTarget_1(data target)
+        {
+
+        }
+
+        private void DisplayTarget_2(data target)
+        {
+
+        }
+
+        private Ellipse MakeEllipse(double x, double y, double height)
+        {
+            Ellipse tmp = new Ellipse();
+            tmp.Stroke = Brushes.Red;
+            tmp.StrokeThickness = 1.0;
+            tmp.Width = 5;
+            tmp.Height = 5;
+            Canvas.SetTop(tmp, height - y - 5);
+            Canvas.SetLeft(tmp, x);
+            return tmp;
+        }
+
+        private Rectangle MakeRectangle(double x, double y, double height)
+        {
+            Rectangle tmp = new Rectangle();
+            tmp.Stroke = Brushes.Blue;
+            tmp.StrokeThickness = 1.0;
+            tmp.Width = 5;
+            tmp.Height = 5;
+            Canvas.SetTop(tmp, height - y - 5);
+            Canvas.SetLeft(tmp, x);
+            return tmp;
+        }
+
+        public long count = 0;
+        public double gravity = -9.8;
+        private void MoveTarget()
+        {
+            Target = new Point(Target.X + Velocity * Math.Cos(Math.PI / 180.0 * Direction), Target.Y + (Velocity * Math.Sin(Math.PI / 180.0 * Direction) + count * gravity));
+            count++;
+        }
+
     }
 
     public enum FilterType
